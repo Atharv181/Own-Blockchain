22f97d3d5d487f9f9f2515bea1240e7ef955cc4f
Atharv181 <atharvbobade181@gmail.com>
2023-03-04 19:36:01 +0530

    Genesis block Added

--- block.js
+++ block.js
@@ -1,11 +1,27 @@
-class Block{
-    constructor({timestamp,prevHash,hash,data}){
-        this.timestamp = timestamp;
-        this.prevHash = prevHash;
-        this.hash = hash;
-        this.data = data;
-    }
+const {GENESIS_DATA} = require('./config')
+
+class Block {
+  constructor({ timestamp, prevHash, hash, data }) {
+    this.timestamp = timestamp;
+    this.prevHash = prevHash;
+    this.hash = hash;
+    this.data = data;
+  }
+
+  static genesis(){
+    return new this(GENESIS_DATA);
+  }
 }
 
-const block1 = new Block({timestamp:'1234',prevHash:'0xabc',hash:'0xdef',data:'Hello World'});
-console.log(block1);
+const block1 = new Block({
+  timestamp: 1234,
+  prevHash: "0xabc",
+  hash: "0xdef",
+  data: "Hello World",
+});
+
+const genesisBlock = Block.genesis();
+//console.log(genesisBlock)
+
+//console.log(block1);
+
---
+++ config.js
@@ -0,0 +1,7 @@
+const GENESIS_DATA ={
+    timestamp : 181,
+    prevHash : '0x000',
+    hash : '0x123',
+    data:"Atharv",
+};
+module.exports = { GENESIS_DATA };
